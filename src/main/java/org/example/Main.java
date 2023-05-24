c8c25dc3fcc9f3952fb86e1d03dd7143fc284fb7
Gleb Lipay <gleb.lipai@yandex.ru>
2023-05-24 21:08:34 +0300

    change greeting to 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello QA Guru!111111");
+        System.out.println("Hello 2222222");
     }
 }
